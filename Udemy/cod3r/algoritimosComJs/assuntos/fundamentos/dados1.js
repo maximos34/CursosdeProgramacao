f280840d93cd662bd45c263b88fcb68160e73ad1
pedro falcao <maximosjoselages@gmail.com>
2022-08-18 10:02:12 -0300

    novas aulas
    conteudo das duas ultimas aulas assistidas

--- Udemy/cod3r/algoritimosComJs/assuntos/fundamentos/dados1.js
+++ Udemy/cod3r/algoritimosComJs/assuntos/fundamentos/dados1.js
@@ -2,3 +2,17 @@
 
 // trabalhando com dados no JavaScript
 
+
+//variveis = são espaços alocados na memoria onde se pode armazenar informações por um periodo de tempo e são mutaveis , porem ao usar a palavra reservada const a mesma variavel n pode ser alterada 
+
+let produto = "caneta";
+let quantidade = 10 ;
+let preco =6.4 ;
+let imposto = 1.5;
+let precofinal= preco + imposto
+
+console.log(produto); 
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precofinal);
--- Udemy/cod3r/algoritimosComJs/assuntos/fundamentos/dados2.js
+++ Udemy/cod3r/algoritimosComJs/assuntos/fundamentos/dados2.js
@@ -1,5 +1,22 @@
-const preco = 19.9
-const desconto = 0.4
-const precoComDesconto = preco*(1-desconto)
 
-console.log(precoComDesconto)
+
+/* anatomia das variaveis 
+
+palavra reservada + identificador ( nome da variavel ) + conteudo da variavel
+
+Palavras resevadas :
+
+var= n usada mais
+let = usada mais atualmente e permite a modificação da variavel 
+const = define a variavel se torne uma constante ( uma variavel que n se pode alterar ) 
+
+
+*/
+
+
+let preco = 19.90;
+let desconto = 0.4;
+
+console.log(19.90 * 0.6)
+
+console.log(preco * (1 - desconto))
